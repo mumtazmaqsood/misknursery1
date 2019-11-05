964cee08e8883c8cc992fb67b7d44f5b6ee16d4f
mumtaz maqsood <mumtaz.maqsood@yahoo.com>
2019-11-05 19:31:15 +0100

    left menus FROM 'THE EMBRACE COLLECTOIN' TO 'CONTACT US'

--- test.py
+++ test.py
@@ -50,3 +50,107 @@ class Navigation():
 #----------------------------------------------------------------------------------------------------------------------
                     # END OF SHOP COLLECTION -> SUB MENUS'
 # ---------------------------------------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------------------------------------------
+#        left menus FROM 'THE EMBRACE COLLECTOIN' TO 'CONTACT US'
+# -------------------------------------------------------------------------------------------------------------------
+
+    def left_nav1(self):
+
+        side_nav_Menu = driver.find_elements(By.XPATH, "//ul[@class='site-nav side_nav']//li")
+        # need iterate from index 6 - 14, left menu items from 'The Embrace Collections, ---> Contact us'
+        # 7 is a fix value , dont change otherwise error will occure bcz it iterate on 6-14, which are 8 menus
+        # so total len is 15-7 = 8
+        for i in range(len(side_nav_Menu)-7):
+            # the menu 'the Embrace collection' is on the 6th position
+            i = i + 6
+            driver.find_elements(By.XPATH, "//ul[@class='site-nav side_nav']//li")[i].click()
+            sleep(2)
+
+            emb_Products = driver.find_elements(By.XPATH, "//div[@class = 'grid active-list "
+                                                          "grid-list-view grid--uniform grid--view-items']"
+                                                          "//div[@class='grid-view-item custom-grid-view']")
+            sleep(2)
+            # click on product mostly 5 products on each page(choose 0 - 4)
+            for emb_Products_Index in range(len(emb_Products)-2):
+                emb_Products = driver.find_elements(By.XPATH,
+                                                    "//div[@class = 'grid active-list grid-list-view "
+                                                    "grid--uniform grid--view-items']"
+                                                    "//div[@class='grid-view-item custom-grid-view']")
+                emb_Products[emb_Products_Index].click()
+                sleep(2)
+                # --------------------------------------------------------------------------------------------------------------
+                # adding to shopping cart total 15 fields(0 -- 14), on 6th postion , there is 'add to cart' button,
+                # thats why loop start 0 --- (15)-9,
+                add_to_cart = driver.find_elements(By.XPATH, "//div[@class = 'grid product-single']"
+                                                             "//div[@class='product-single__meta']//div")
+                for index in range(len(add_to_cart)-9):
+                    sleep(2)
+                    add_to_cart = driver.find_elements(By.XPATH,
+                                         "//div[@class = 'grid product-single']//div[@class="
+                                         "'product-single__meta']//div")
+                    # select value from dropdown menu 'Type'
+                    type_dropdown = Select(driver.find_element_by_id("SingleOptionSelector-0"))
+                    type_all_options = len(type_dropdown.options)
+                    if type_all_options > 1:
+                        type_dropdown.select_by_index(1)
+                    else:
+                        type_dropdown.select_by_index(0)
+
+                    # select value form dropdown menu 'Size'
+                    size_dropdown = Select(driver.find_element_by_id("SingleOptionSelector-1"))
+                    size_all_options = len(size_dropdown.options)
+                    if size_all_options > 1:
+                        size_dropdown.select_by_index(1)
+                    else:
+                        size_dropdown.select_by_index(0)
+
+                    #--------------------------------------------------------------------------------------------
+                    # NEED TO CORRECT BELOW CODE BEACUSE 'THE EMBRACE COLLECTION' 2ND PRODUCT HAS NOT COLOR FIELD
+                    # THAT'S WHY THIS CODE GIVES ERROR OTHERWISE FINE
+                    # --------------------------------------------------------------------------------------------
+                    # select value form dropdown menu 'Colors'
+                    # colorDropDown = driver.find_element_by_xpath("//div[@class = 'grid product-single']"
+                    #                              "//div[@class='product-single__meta']"
+                    #                              "//div[@class='selector-wrapper js product-form__item']"
+                    #                              "//select[@id='SingleOptionSelector-2']")
+                    #
+                    # if not colorDropDown:
+                    #     print("Not Found")
+                    # else:
+                    # color_dropdown = Select(driver.find_element_by_id("SingleOptionSelector-2"))
+                    # colors_all_options = color_dropdown.options
+                    # color_dropdown.select_by_index(1)
+                    sleep(2)
+                    # add_to_cart[5] mostly add to cart button , it is not working thats why need if statment
+                    if emb_Products_Index == 1 and add_to_cart[index] == add_to_cart[4]:
+                        driver.find_element_by_xpath("//div[@class = 'grid product-single']"
+                                                     "//div[@class='product-single__meta']"
+                                                     "//div[@class='list-inline-cart medium-"
+                                                     "-one-half product-form__item product-form__item--submit']"
+                                                     "//button[@name='add']").click()
+                        sleep(2)
+                        driver.back()
+
+                    elif add_to_cart[index] == add_to_cart[5]:
+                        driver.find_element_by_xpath("//div[@class = 'grid product-single']"
+                                                     "//div[@class='product-single__meta']"
+                                                     "//div[@class='list-inline-cart medium-"
+                                                     "-one-half product-form__item product-form__item--submit']"
+                                                     "//button[@name='add']").click()
+                        sleep(2)
+                        driver.back()
+                    else:
+                        add_to_cart[index].click()
+                    sleep(2)
+
+
+                driver.back()
+                sleep(2)
+            # driver.find_element_by_xpath("//i[@class='fa fa-list']").click()
+            driver.back()
+#----------------------------------------------------------------------------------------------------------------------
+                                            # END OF LEFT MENU
+# ---------------------------------------------------------------------------------------------------------------
+
